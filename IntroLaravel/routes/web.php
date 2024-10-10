9d79c04f30713aef3821cceeef22b2045314dd70
Nyxxs12222 <122043017@upq.edu.mx>
2024-10-09 18:58:43 -0600

    Practica controlladores

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -4,11 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\controladorVistas;
 
 route::get('/',[controladorVistas::class, 'home'])->name('rutainicio');
-route::get('/form',[controladorVistas::class, 'formulario'])->name('rutaform');
-route::get('/cleintes',[controladorVistas::class, 'consulta'])->name('rutaclientes');
+route::get('/form.',[controladorVistas::class, 'formulario'])->name('rutaform');
+route::get('/clientes.',[controladorVistas::class, 'consulta'])->name('rutaclientes');
 
 
-
+Route::view('/componentes', 'componentes')->name('rutacomponentes'); 
 
 /* Route::get('/', function () {
     return view('welcome');
