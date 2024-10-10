bc1a679bea9a7ad1ad9a4cba265f1088208aa8a1
Nyxxs12222 <122043017@upq.edu.mx>
2024-10-09 18:52:23 -0600

    Practica 3.1

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+route::get('/',[controladorVistas::class, 'home'])->name('rutainicio');
+route::get('/form',[controladorVistas::class, 'formulario'])->name('rutaform');
+route::get('/cleintes',[controladorVistas::class, 'consulta'])->name('rutaclientes');
+
+
+
 
 /* Route::get('/', function () {
     return view('welcome');
-}); */
+}); *//* 
 
 Route::view('/', 'inicio')->name('rutainicio');
 Route::view('/form', 'formulario')->name('rutaform');
 Route::view('/clientes', 'clientes')->name('rutaclientes');
 
-Route::view('/componentes', 'componentes')->name('rutacomponentes');
+Route::view('/componentes', 'componentes')->name('rutacomponentes'); */
