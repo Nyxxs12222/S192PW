d438937da66cb107ccb1a9dcb32c84e3c7455625
Nyxxs12222 <122043017@upq.edu.mx>
2024-11-04 19:52:11 -0600

    Primer Avance 7:50

---
+++ 2doP/app/Http/Controllers/controladorVistas.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\validadorClientes;
+use Illuminate\Http\Request;
+
+class controladorVistas extends Controller
+{
+    public function home(){
+        return view('inicio');
+    }
+    public function formulario(){
+        return view('formulario');
+    }
+    
+    public function consulta(){
+        return view('clientes');
+    }
+
+    public function procesarCliente(validadorClientes $peticionValidada){
+
+        $usuario = $peticionValidada->input('txtnombre');
+
+        session()->flash('exito','se guardo el usuario '.$usuario);
+
+        return to_route('rutaform');
+    }
+}
